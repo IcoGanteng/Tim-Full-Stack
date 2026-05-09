e5c6475d9f04d77d25359c56b03d91e740aea19f
Timoty25 <hizkiagea76@gmail.com>
2026-05-09 13:45:36 +0700

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,1 +1,8 @@
-// Updated reusable UI component 
+export default function Navbar() {
+  return (
+    <nav>
+      <h1>Health Tracker</h1>
+      <p>Track Your Daily Health</p>
+    </nav>
+  );
+}// Updated reusable UI component 
